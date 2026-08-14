8b2b7731ea7588237538cca4ca1b7dcfdb02ee3e
aliha55anch <chali37125@gmail.com>
2026-08-14 23:08:15 +0500

    Admin Owner access bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const express = require('express')
 const path = require('path')
-const dotenv = require('dotenv')
 const cors = require('cors')
 const connectDB = require('./config/db')
 const webhookRoutes = require('./routes/webhookRoutes')
@@ -15,8 +17,6 @@ const offerRoutes = require('./routes/offerRoutes')
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 const { clerkAuth, syncClerkUser } = require('./middleware/authMiddleware')
 const { cleanupExpiredBookings } = require('./controllers/bookingController')
-
-dotenv.config()
 
 connectDB()
 
