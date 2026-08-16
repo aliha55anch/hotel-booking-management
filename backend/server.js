ddf6ccc5a587f1990fff7393dc3eb88a73c11371
aliha55anch <chali37125@gmail.com>
2026-08-16 23:05:29 +0500

    Made changes in Backend

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ const start = async () => {
   app.use(notFound)
   app.use(errorHandler)
 
-  const PORT = process.env.PORT || 5500
+  const PORT = process.env.PORT 
 
   const server = app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`)
